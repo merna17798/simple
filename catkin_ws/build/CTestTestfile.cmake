aa3f6f3be9230fc6c44eb572c90e69db19a4aa32
merna17798 <mernasaleh1998@gmail.com>
2020-12-26 15:31:27 +0200

    all

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/merna/catkin_ws/src
+# Build directory: /home/merna/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("first_package")
+subdirs("simple_arm")
---
+++ catkin_ws/build/first_package/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/merna/catkin_ws/src/first_package
+# Build directory: /home/merna/catkin_ws/build/first_package
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
